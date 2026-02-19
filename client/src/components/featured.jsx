48ea104bf90482cd7f29583babd35f3fb55804eb
Jon Blackburn <jon.blackburn@mongodb.com>
2026-02-18 17:49:51 -0700

    Lesson 11: Featured Products complete.

--- client/src/components/featured.jsx
+++ client/src/components/featured.jsx
@@ -1,60 +1,51 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import productImg from '../images/productImg.png';
+import axios from "axios";
 
 const Featured = () => {
-  return (
-    <>
-      <div id="gallery-head">
-        <h1> Gallery </h1>
-      </div>
-      <div id="card-container">
-      <div class="featured-card">
-          <img
-            className="img"
-            src={productImg}
-            alt=""
-          />
-          <h3>Add a product here</h3>
-        </div>
+    const [products, setProducts] = useState([]);
 
-        <div class="featured-card">
-          <img
-            className="img"
-            src={productImg}
-            alt=""
-          />
-          <h3>Add a product here</h3>
-        </div>
-
-        <div class="featured-card">
-          <img
-            className="img"
-            src={productImg}
-            alt=""
-          />
-          <h3>Add a product here</h3>
-        </div>
-
-        <div class="featured-card">
-          <img
-            className="img"
-            src={productImg}
-            alt=""
-          />
-          <h3>Add a product here</h3>
-        </div>
-
-        <div class="featured-card">
-          <img
-            className="img"
-            src={productImg}
-            alt=""
-          />
-          <h3>Add a product here</h3>
-        </div>
-      </div>
-    </>
-  );
+    useEffect(() => {
+        const getFeaturedProducts = async () => {
+            try {
+                const response = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/products`);
+                console.log("response", response);
+                const products = response.data.rows;
+                setProducts(products.slice(0, 5));
+            } catch (e) {
+                console.error("Error retrieving featured products", e);
+                setProducts([]);
+            }
+        }
+        getFeaturedProducts().then(() => {
+        });
+    }, [])
+    return (
+        <>
+            <div id="gallery-head">
+                <h1> Gallery </h1>
+            </div>
+            <div id="card-container">
+                {
+                    products.map((product) => {
+                        return (
+                            <div className="featured-card">
+                                <div hidden={true}>{product.id}</div>
+                                <img
+                                    className="img"
+                                    src={product.image_url}
+                                    alt=""
+                                />
+                                <h3>{product.name}</h3>
+                                <p>{product.description}</p>
+                                <p>{product.price}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </>
+    );
 };
 
 export default Featured;
